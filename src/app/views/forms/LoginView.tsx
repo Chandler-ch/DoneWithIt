f8f81f3f0ce651112b0871ece46442e9ae4c69c3
Chandler-ch <rchandler@mail.ch>
2026-06-26 14:53:52 +0200

    Added basic logic to loginView

--- src/app/views/forms/LoginView.tsx
+++ src/app/views/forms/LoginView.tsx
@@ -1,10 +1,14 @@
 import AppButton from "@/app/components/AppButton";
 import AppTextInput from "@/app/components/AppTextInput";
 import defaultStyles from "@/app/config/defaultStyles";
+import { useState } from "react";
 import { Image, StyleSheet, View } from "react-native";
 import FormScreen from "../../components/FormScreen";
 
 export default function Login() {
+  const [email, setEmail] = useState(String);
+  const [password, setPasstword] = useState(String);
+
   return (
     <FormScreen>
       <View style={styles.logoContainer}>
@@ -19,6 +23,7 @@ export default function Login() {
           autoCorrect={false}
           keyboardType="email-address"
           icon="email"
+          onChangeText={(text) => setEmail(text)}
           placeholder="Email"
           textContentType="emailAddress"
         />
@@ -27,10 +32,11 @@ export default function Login() {
           autoCorrect={false}
           textContentType="password"
           icon="lock"
+          onChangeText={(text) => setPasstword(text)}
           placeholder="Password"
           secureTextEntry
         />
-        <AppButton title="Login" />
+        <AppButton title="Login" onPress={() => console.log(email, password)} />
       </View>
     </FormScreen>
   );
